6fc52d1aa1f5ef61d5dc8b7b87c5c9205d5bbfd2
Milos Ribar <milosribar@yahoo.com>
2020-09-13 19:35:22 +0200

    Keycloak with Spring Security

---
+++ EventSorcerer/src/main/java/rixon/event/sourcing/EventSorcerer/keycloak/KeycloakData.java
@@ -0,0 +1,10 @@
+package rixon.event.sourcing.EventSorcerer.keycloak;
+
+import org.springframework.context.annotation.Configuration;
+
+import rixon.event.sourcing.EventSorcerer.configuration.ConfigProperties;
+
+@Configuration
+public class KeycloakData extends ConfigProperties{
+	
+}
